56bbfa6d8d01918eaf61dca5e17519800cee1682
dylanonelson <dylanonelson@gmail.com>
2015-05-12 15:29:56 -0400

    routes for login, logout, signup, and homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
 
+  root to: 'users#show'
+
+  resources :users, only: [:new, :create, :index, :show]
+  resources :sessions, only: [:new, :create, :destroy]
+
 end
