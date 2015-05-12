eb705d9a167bdcb2413a6c6260f8dcf4073a2bc9
dylanonelson <dylanonelson@gmail.com>
2015-05-12 18:13:47 -0400

    generated Backbone files

---
+++ app/assets/javascripts/medium_clone.js
@@ -0,0 +1,13 @@
+window.MediumClone = {
+  Models: {},
+  Collections: {},
+  Views: {},
+  Routers: {},
+  initialize: function() {
+    alert('Hello from Backbone!');
+  }
+};
+
+$(document).ready(function(){
+  MediumClone.initialize();
+});
