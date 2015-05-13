fd19cbf390354e43aa0b2a7b758e4a4726685bca
dylanonelson <dylanonelson@gmail.com>
2015-05-13 14:42:25 -0400

    story form handles new story creation

--- app/assets/javascripts/views/story_form.js
+++ app/assets/javascripts/views/story_form.js
@@ -1,5 +1,9 @@
 MediumClone.Views.StoryForm = Backbone.View.extend({
 
+  tagName : 'form',
+
+  className : 'story-form',
+
   template : JST['story_form'],
 
   render : function () {
@@ -9,6 +13,20 @@ MediumClone.Views.StoryForm = Backbone.View.extend({
 
     this.$el.html(rendered);
     return this;
+  },
+
+  events : {
+    "click #submit-story" : "submitStory",
+  },
+
+  submitStory : function (event) {
+    event.preventDefault();
+    var attr = this.$el.serializeJSON();
+    this.model.save(attr, {
+      success : function () {
+        Backbone.history.navigate('', { trigger : true });
+      },
+    });
   },
 
 })
