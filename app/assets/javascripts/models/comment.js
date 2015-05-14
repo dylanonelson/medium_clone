3a63fc2efcc882c6b8735e29d5ac7a6d9b920ab2
dylanonelson <dylanonelson@gmail.com>
2015-05-14 18:07:23 -0400

    Backbone comment model fetches and parses

--- app/assets/javascripts/models/comment.js
+++ app/assets/javascripts/models/comment.js
@@ -1,8 +1,15 @@
 MediumClone.Models.Comment = Backbone.Model.extend({
 
+  urlRoot : 'api/comments',
+
   initialize : function (options) {
-    this.set('commenter', new MediumClone.Models.User(options.commenter));
-    this.get('commenter').fetch();
+    options.commenter && this.set('commenter', new MediumClone.Models.User(options.commenter));
+  },
+
+  parse : function (payload) {
+    payload.commenter && this.set('commenter', new MediumClone.Models.User(payload.commenter));
+    delete payload.commenter;
+    return payload;
   },
 
 })
