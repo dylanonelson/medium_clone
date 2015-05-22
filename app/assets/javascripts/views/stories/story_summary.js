efe69f3e0f8350e502dd3b337cd197c7399986ef
dylanonelson <dylanonelson@gmail.com>
2015-05-22 12:00:45 -0400

    break story summaries into separate view

--- app/assets/javascripts/views/stories/story_summary.js
+++ app/assets/javascripts/views/stories/story_summary.js
@@ -1,14 +1,13 @@
 MediumClone.Views.StorySummary = Backbone.View.extend({
 
-  template : JST['stories/story_show'],
+  template : JST['stories/story_summary'],
 
   render : function () {
     var summaryBody = this.model.getSummary();
 
     var rendered = this.template({
       story : this.model,
-      author : this.model.get('author'),
-      body : summaryBody,
+      storySummary : summaryBody,
     });
 
     this.$el.html(rendered);
