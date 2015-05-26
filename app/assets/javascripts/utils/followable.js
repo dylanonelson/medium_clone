95d8b42ebdb718c9d6228053c367a07f6fcd3411
dylanonelson <dylanonelson@gmail.com>
2015-05-26 15:48:07 -0400

    fix following and commenting bug when requiring sign in for signed in users

--- app/assets/javascripts/utils/followable.js
+++ app/assets/javascripts/utils/followable.js
@@ -15,31 +15,35 @@ MediumClone.Mixins.Followable = {
     var type
     var thisModel = this;
 
-    if (!MediumClone.router._requireSignedIn(function () {
-      thisModel.fetch({
-        success : function() {
-          if (thisModel.get('following')) {
-            type = 'DELETE';
-          } else {
-            type = 'POST';
-          }
-
-          $.ajax({
-            url : 'api/follow',
-            type : type,
-            data : {
-              follow: {
-                followable_id : thisModel.id,
-                followable_type : thisModel.followableType,
-              }
-            },
-            success : completionCallback,
-          });
-        },
+    if (!MediumClone.router._requireSignedIn(function() {
+      thisModel.toggleFollow(function () {
+        completionCallback();
+        MediumClone.router.showUser(thisModel.id);
+        Backbone.history.navigate('users/' + thisModel.id);
       });
-      MediumClone.router.profile();
-      Backbone.history.navigate('');
     })) { return; }
+
+    thisModel.fetch({
+      success : function() {
+        if (thisModel.get('following')) {
+          type = 'DELETE';
+        } else {
+          type = 'POST';
+        }
+
+        $.ajax({
+          url : 'api/follow',
+          type : type,
+          data : {
+            follow: {
+              followable_id : thisModel.id,
+              followable_type : thisModel.followableType,
+            }
+          },
+          success : completionCallback,
+        });
+      },
+    });
   },
-  
+
 }
--- app/assets/javascripts/views/comments/comment_form.js
+++ app/assets/javascripts/views/comments/comment_form.js
@@ -23,12 +23,13 @@ MediumClone.Views.CommentForm = Backbone.View.extend({
   },
 
   submitComment : function (event) {
-    event.preventDefault();
+    event && event.preventDefault();
 
     var thisView = this;
-    
-    if(!MediumClone.router._requireSignedIn(function () {
-      thisView.model.set('body', thisView.$el.serializeJSON().body);
+
+    if (!MediumClone.router._requireSignedIn(this.submitComment.bind(this))) { return; }
+
+    thisView.model.set('body', thisView.$el.serializeJSON().body);
       thisView.model.save({}, {
         success : function () {
           thisView.collection.add(thisView.model);
@@ -36,8 +37,6 @@ MediumClone.Views.CommentForm = Backbone.View.extend({
       });
       MediumClone.router.showStory(thisView.model.get('story_id'));
       Backbone.history.navigate('stories/' + thisView.model.get('story_id'));
-    })) { return; }
-      
   },
 
 })
